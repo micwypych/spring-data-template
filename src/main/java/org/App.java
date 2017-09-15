8ba631f549363c8f0258677cae6a8e74c3cb46cd
Michał Wypych <mwypych@agh.edu.pl>
2017-09-15 18:35:12 +0200

    Adds real time monitoring code

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,5 +1,9 @@
 package org;
 
+import com.netflix.servo.monitor.Counter;
+import com.netflix.servo.monitor.Monitors;
+import com.netflix.servo.monitor.Stopwatch;
+import com.netflix.servo.monitor.Timer;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +15,8 @@ public class App {
     @Autowired
     private CustomerRepository repository;
 
+    private final Counter createdCusomers = Monitors.newCounter("createdCustomers");
+
     public App(CustomerRepository repository) {
         this.repository = repository;
     }
@@ -18,14 +24,18 @@ public class App {
     public String getGreeting() {
         Customer mark = new Customer("Mark", "Davids");
         repository.save(mark);
+        createdCusomers.increment();
         Iterable<Customer> customers = repository.findAll();
         return customers.toString();
     }
 
     public static void main(String[] args) {
+        Timer latency = Monitors.newTimer("latency");
+        Stopwatch stopwatch = latency.start();
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         App app = context.getBean(App.class);
         Logger log = context.getBean(Logger.class);
         log.info(app.getGreeting());
+        stopwatch.stop();
     }
 }
